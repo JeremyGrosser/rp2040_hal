340fb734b0cb16276bf64162bfcd4e68eb0813c2
Jeremy Grosser <jeremy@synack.me>
2022-02-09 13:25:43 -0800

    Move PIO Audio_I2S driver from pico_bsp

---
+++ src/drivers/rp-pio-audio_i2s.adb
@@ -0,0 +1,129 @@
+--
+--  Copyright 2021 (C) Jeremy Grosser <jeremy@synack.me>
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
+with Interfaces; use Interfaces;
+with RP.PIO.Audio_I2S_PIO;
+with HAL; use HAL;
+with RP; use RP;
+
+package body RP.PIO.Audio_I2S is
+
+   procedure Program_Init
+      (This   : in out I2S_Device;
+       Offset : PIO_Address)
+   is
+   begin
+      This.Config := Default_SM_Config;
+      Set_Out_Pins (This.Config, This.Data.Pin, 1);
+      Set_Sideset_Pins (This.Config, This.BCLK.Pin);
+      Set_Sideset (This.Config,
+         Bit_Count => 2,
+         Optional  => False,
+         Pindirs   => False);
+      Set_Out_Shift (This.Config,
+         Shift_Right    => False,
+         Autopull       => True,
+         Pull_Threshold => 32);
+      Set_Wrap (This.Config,
+          Wrap        => Offset + RP.PIO.Audio_I2S_PIO.Audio_I2s_Wrap,
+          Wrap_Target => Offset + RP.PIO.Audio_I2S_PIO.Audio_I2s_Wrap_Target);
+
+      SM_Initialize (This.PIO.all, This.SM, Offset, This.Config);
+
+      Set_Pin_Direction (This.PIO.all, This.SM, This.Data.Pin, Output);
+      Set_Pin_Direction (This.PIO.all, This.SM, This.BCLK.Pin, Output);
+      Set_Pin_Direction (This.PIO.all, This.SM, This.LRCLK.Pin, Output);
+
+      Execute (This.PIO.all, This.SM, PIO_Instruction (Offset + RP.PIO.Audio_I2S_PIO.Offset_entry_point));
+   end Program_Init;
+
+   overriding
+   procedure Set_Frequency
+      (This      : in out I2S_Device;
+       Frequency : HAL.Audio.Audio_Frequency)
+   is
+      Sample_Rate       : constant Hertz :=
+         Hertz (HAL.Audio.Audio_Frequency'Enum_Rep (Frequency));
+      Sample_Bits       : constant := 16;
+      Cycles_Per_Sample : constant := 2;
+   begin
+      Set_Clock_Frequency (This.Config, Sample_Rate * Sample_Bits * This.Channels * Cycles_Per_Sample);
+      Set_Config (This.PIO.all, This.SM, This.Config);
+   end Set_Frequency;
+
+   procedure Initialize
+      (This      : in out I2S_Device;
+       Frequency : HAL.Audio.Audio_Frequency;
+       Channels  : Channel_Count := 1)
+   is
+      DMA_Config : DMA_Configuration :=
+         (Increment_Read => True,
+          others         => <>);
+      SM_Offset  : constant PIO_Address := 0;
+      AF         : constant RP.GPIO.GPIO_Function := RP.PIO.GPIO_Function (This.PIO.all);
+   begin
+      RP.DMA.Enable;
+      This.Data.Configure (Output, Pull_Both, AF);
+      This.BCLK.Configure (Output, Pull_Both, AF);
+      This.LRCLK.Configure (Output, Pull_Both, AF);
+
+      Enable (This.PIO.all);
+      Load (This.PIO.all,
+          Prog   => RP.PIO.Audio_I2S_PIO.Audio_I2s_Program_Instructions,
+          Offset => SM_Offset);
+
+      Program_Init (This, SM_Offset);
+      Set_Frequency (This, Frequency);
+      Set_Enabled (This.PIO.all, This.SM, True);
+
+      DMA_Config.Trigger := DMA_Request_Trigger'Val
+         (DMA_Request_Trigger'Pos (PIO0_TX0) + (This.PIO.Num * 8) + Natural (This.SM));
+      if Channels = 1 then
+         DMA_Config.Data_Size := Transfer_16;
+      else
+         DMA_Config.Data_Size := Transfer_32;
+      end if;
+      RP.DMA.Configure (This.DMA_Channel, DMA_Config);
+   end Initialize;
+
+   overriding
+   procedure Transmit
+      (This : in out I2S_Device;
+       Data : HAL.Audio.Audio_Buffer)
+   is
+      Count : HAL.UInt32 := Data'Length;
+   begin
+      --  Wait for previous DMA transfer to finish before modifying the buffer.
+      while Busy (This.DMA_Channel) loop
+         null;
+      end loop;
+
+      This.Buffer (1 .. Data'Length) := Data;
+
+      if This.Channels > 1 then
+         Count := Data'Length / 2;
+      end if;
+
+      RP.DMA.Start
+         (Channel => This.DMA_Channel,
+          From    => This.Buffer'Address,
+          To      => TX_FIFO_Address (This.PIO.all, This.SM),
+          Count   => Count);
+   end Transmit;
+
+   overriding
+   procedure Receive
+      (This : in out I2S_Device;
+       Data : out HAL.Audio.Audio_Buffer)
+   is
+      D : UInt32;
+   begin
+      for I in Data'Range loop
+         Get (This.PIO.all, This.SM, D);
+         Data (I) := Integer_16 (D);
+      end loop;
+   end Receive;
+
+end RP.PIO.Audio_I2S;
---
+++ src/drivers/rp-pio-audio_i2s.ads
@@ -0,0 +1,77 @@
+--
+--  Copyright 2021 (C) Jeremy Grosser <jeremy@synack.me>
+--
+--  SPDX-License-Identifier: BSD-3-Clause
+--
+with RP.PIO; use RP.PIO;
+with RP.DMA; use RP.DMA;
+with RP.GPIO;
+
+with HAL.Audio;
+with HAL;
+
+package RP.PIO.Audio_I2S is
+
+   --  There is only one LRCLK signal, so any more than two channels will need
+   --  some external multiplexing logic.
+   subtype Channel_Count is Positive range 1 .. 2;
+
+   type I2S_Device
+      (Data        : not null access RP.GPIO.GPIO_Point;
+       BCLK        : not null access RP.GPIO.GPIO_Point;
+       LRCLK       : not null access RP.GPIO.GPIO_Point;
+       PIO         : not null access RP.PIO.PIO_Device'Class;
+       SM          : RP.PIO.PIO_SM;
+       Channels    : Channel_Count;
+       DMA_Channel : DMA_Channel_Id;
+       Buffer_Size : Positive)
+   is limited new HAL.Audio.Audio_Stream with private;
+
+   procedure Initialize
+      (This      : in out I2S_Device;
+       Frequency : HAL.Audio.Audio_Frequency;
+       Channels  : Channel_Count := 1);
+
+   overriding
+   procedure Set_Frequency
+      (This      : in out I2S_Device;
+       Frequency : HAL.Audio.Audio_Frequency);
+
+   --  Copies Data into the buffer and starts a DMA transfer. If a DMA transfer
+   --  is already in progress, Transmit will wait for it to finish before
+   --  modifying the buffer.
+   overriding
+   procedure Transmit
+      (This : in out I2S_Device;
+       Data : HAL.Audio.Audio_Buffer)
+    with Pre => Data'Length <= This.Buffer_Size;
+
+   --  The PIO program currently does not support I2S receive. This function
+   --  will block on RX FIFO forever.
+   overriding
+   procedure Receive
+      (This : in out I2S_Device;
+       Data : out HAL.Audio.Audio_Buffer);
+
+private
+
+   --  Init copied from audio_i2s.pio and translated to Ada
+   procedure Program_Init
+      (This   : in out I2S_Device;
+       Offset : PIO_Address);
+
+   type I2S_Device
+      (Data        : not null access RP.GPIO.GPIO_Point;
+       BCLK        : not null access RP.GPIO.GPIO_Point;
+       LRCLK       : not null access RP.GPIO.GPIO_Point;
+       PIO         : not null access PIO_Device'Class;
+       SM          : PIO_SM;
+       Channels    : Channel_Count;
+       DMA_Channel : DMA_Channel_Id;
+       Buffer_Size : Positive)
+   is limited new HAL.Audio.Audio_Stream with record
+      Config : PIO_SM_Config;
+      Buffer : aliased HAL.Audio.Audio_Buffer (1 .. Buffer_Size);
+   end record;
+
+end RP.PIO.Audio_I2S;
