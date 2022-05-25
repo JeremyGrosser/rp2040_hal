ac6e9efd02f66374a123c6fa97cb10094a305f0d
Fabien Chouteau <fabien.chouteau@gmail.com>
2022-05-25 21:55:04 +0200

    RP.USB_Device: update for new USB stack API

--- src/drivers/rp-usb_device.ads
+++ src/drivers/rp-usb_device.ads
@@ -33,38 +33,34 @@ package RP.USB_Device is
 
    overriding
    function Request_Buffer
-      (This          : in out USB_Device_Controller;
-       Ep            : USB.EP_Addr;
-       Len           : UInt11;
-       Min_Alignment : UInt8 := 1)
+     (This : in out USB_Device_Controller;
+      Ep   : USB.EP_Addr;
+      Len  : USB.Packet_Size)
        return System.Address;
 
    overriding
    function Valid_EP_Id
       (This : in out USB_Device_Controller;
-       EP   : USB.EP_Id)
+       Ep   : USB.EP_Id)
        return Boolean;
 
    overriding
-   procedure EP_Write_Packet
+   procedure EP_Send_Packet
       (This : in out USB_Device_Controller;
        Ep   : USB.EP_Id;
-       Addr : System.Address;
-       Len  : UInt32);
+       Len  : USB.Packet_Size);
 
    overriding
    procedure EP_Setup
       (This     : in out USB_Device_Controller;
        Ep       : USB.EP_Addr;
-       Typ      : USB.EP_Type;
-       Max_Size : UInt16);
+       Typ      : USB.EP_Type);
 
    overriding
    procedure EP_Ready_For_Data
       (This     : in out USB_Device_Controller;
        Ep       : USB.EP_Id;
-       Addr     : System.Address;
-       Max_Len  : UInt32;
+       Max_Len  : USB.Packet_Size;
        Ready    : Boolean := True);
 
    overriding
@@ -93,6 +89,7 @@ private
       Addr           : System.Address := System.Null_Address;
       Next_PID       : Boolean := False;
       Buffer_Address : DPRAM_Offset := DPRAM_Offset'Last;
+      Max_Len        : USB.Packet_Size := USB.Packet_Size'First;
    end record;
 
    type Endpoint_Status_Array is array (USB.EP_Id, USB.EP_Dir) of Endpoint_Status;
@@ -105,24 +102,16 @@ private
    use type System.Storage_Elements.Storage_Offset;
    function Allocate_Buffer
       (This      : in out USB_Device_Controller;
-       Size      : Natural;
-       Alignment : Natural)
+       Size      : Natural)
        return DPRAM_Offset
-   with Pre  => Alignment >= 64
-                and Alignment mod 64 = 0
-                and Size <= 1024,
+   with Pre  => Size <= 1024,
         Post => Allocate_Buffer'Result >= 16#180#
                 and (Natural (Allocate_Buffer'Result) + Size) <= Natural (DPRAM_Offset'Last)
-                and (Natural (Allocate_Buffer'Result) mod Alignment) = 0;
+                and (Natural (Allocate_Buffer'Result) mod 64) = 0;
 
    function Endpoint_Buffer_Address
       (Ep : USB.EP_Addr)
       return System.Address;
-
-   procedure Copy_Endpoint_Buffer
-      (This : in out USB_Device_Controller;
-       Num  : USB.EP_Id;
-       Dir  : USB.EP_Dir);
 
    --  See Errata RP2040-E5
    procedure Enumeration_Fix
