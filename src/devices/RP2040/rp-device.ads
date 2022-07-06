60f2546270ac83a9f8a12e3583952b072b114006
Jeremy Grosser <jeremy@synack.me>
2022-07-05 20:37:54 -0700

    RP.Device.I2C_0/1 now point to the RP.I2C package, RP.Device.I2CM_0/1 use RP.I2C_Master. Removed use clauses from RP.Device.

--- src/devices/RP2040/rp-device.ads
+++ src/devices/RP2040/rp-device.ads
@@ -8,10 +8,11 @@ with RP2040_SVD.I2C;
 with RP2040_SVD.UART;
 with RP2040_SVD.ADC;
 with RP2040_SVD;
-with RP.SPI;  use RP.SPI;
-with RP.I2C_Master; use RP.I2C_Master;
-with RP.UART; use RP.UART;
-with RP.PIO;  use RP.PIO;
+with RP.SPI;
+with RP.I2C_Master;
+with RP.I2C;
+with RP.UART;
+with RP.PIO;
 with RP.RTC;
 with RP.Interpolator;
 with RP.USB_Device;
@@ -19,21 +20,28 @@ with RP.Timer;
 with System;
 
 package RP.Device is
-   SPI_0 : aliased SPI_Port (0, RP2040_SVD.SPI.SPI0_Periph'Access);
-   SPI_1 : aliased SPI_Port (1, RP2040_SVD.SPI.SPI1_Periph'Access);
+   SPI_0 : aliased RP.SPI.SPI_Port (0, RP2040_SVD.SPI.SPI0_Periph'Access);
+   SPI_1 : aliased RP.SPI.SPI_Port (1, RP2040_SVD.SPI.SPI1_Periph'Access);
 
-   I2C_0 : aliased I2C_Master_Port (0, RP2040_SVD.I2C.I2C0_Periph'Access);
-   I2C_1 : aliased I2C_Master_Port (1, RP2040_SVD.I2C.I2C1_Periph'Access);
+   --  RP.I2C implements the low level interface and is recommended if you need
+   --  precise control over timing or repeated start conditions.
+   I2C_0 : aliased RP.I2C.I2C_Port (0, RP2040_SVD.I2C.I2C0_Periph'Access);
+   I2C_1 : aliased RP.I2C.I2C_Port (1, RP2040_SVD.I2C.I2C1_Periph'Access);
 
-   UART_0 : aliased UART_Port (0, RP2040_SVD.UART.UART0_Periph'Access);
-   UART_1 : aliased UART_Port (1, RP2040_SVD.UART.UART1_Periph'Access);
+   --  RP.I2C_Master implements the HAL.I2C interface and is recommended if
+   --  your application needs to be portable to other microcontrollers.
+   I2CM_0 : aliased RP.I2C_Master.I2C_Master_Port (0, RP2040_SVD.I2C.I2C0_Periph'Access);
+   I2CM_1 : aliased RP.I2C_Master.I2C_Master_Port (1, RP2040_SVD.I2C.I2C1_Periph'Access);
+
+   UART_0 : aliased RP.UART.UART_Port (0, RP2040_SVD.UART.UART0_Periph'Access);
+   UART_1 : aliased RP.UART.UART_Port (1, RP2040_SVD.UART.UART1_Periph'Access);
 
    PIO0_Periph : aliased RP.PIO.PIO_Peripheral
       with Import, Address => RP2040_SVD.PIO0_Base;
    PIO1_Periph : aliased RP.PIO.PIO_Peripheral
       with Import, Address => RP2040_SVD.PIO1_Base;
-   PIO_0 : aliased PIO_Device (0, PIO0_Periph'Access);
-   PIO_1 : aliased PIO_Device (1, PIO1_Periph'Access);
+   PIO_0 : aliased RP.PIO.PIO_Device (0, PIO0_Periph'Access);
+   PIO_1 : aliased RP.PIO.PIO_Device (1, PIO1_Periph'Access);
 
    INTERP0_Base : constant System.Address := System'To_Address (16#D000_0080#);
    INTERP1_Base : constant System.Address := System'To_Address (16#D000_00C0#);
