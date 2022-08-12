8439c45b3b0265afe7801909c2693908ca2c344d
Jeremy Grosser <jeremy@synack.me>
2022-08-12 12:59:51 -0700

    RP_Interrupts implementation can be selected by alire config

---
+++ src/interrupts/bb_runtimes/rp_interrupts.ads
@@ -0,0 +1,4 @@
+pragma Warnings (Off, """System.BB.Interrupts"" is an internal GNAT unit");
+pragma Warnings (Off, "use of this unit is non-portable and version-dependent");
+with System.BB.Interrupts;
+package RP_Interrupts renames System.BB.Interrupts;
