be476ed667650e3bbb4081b804e5d8a832342183
Daniel King <damaki.gh@gmail.com>
2022-08-21 10:48:01 +0100

    Delete rp_interrupts.ads for bb_runtimes configs

--- src/interrupts/bb_runtimes/rp_interrupts.ads
+++
@@ -1,4 +0,0 @@
-pragma Warnings (Off, """System.BB.Interrupts"" is an internal GNAT unit");
-pragma Warnings (Off, "use of this unit is non-portable and version-dependent");
-with System.BB.Interrupts;
-package RP_Interrupts renames System.BB.Interrupts;
