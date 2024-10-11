3321566af0d3ad426efd57d6811b3c00e1f20be9
Fabien Chouteau <fabien.chouteau@gmail.com>
2024-10-11 09:13:40 +0200

    RP.PIO.Touch_Sense: Add capacitive charge mode
    Anticipating the support of rp2350, this new mode is a workaround for
    the rp2350 e9 eratum (pin pull-down lock).

--- src/drivers/rp-pio-touch_sense.adb
+++ src/drivers/rp-pio-touch_sense.adb
@@ -3,7 +3,8 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with RP.PIO.Touch_Sense_PIO;
+with RP.PIO.Touch_Sense_Discharge_PIO; use RP.PIO.Touch_Sense_Discharge_PIO;
+with RP.PIO.Touch_Sense_Charge_PIO; use RP.PIO.Touch_Sense_Charge_PIO;
 
 package body RP.PIO.Touch_Sense is
 
@@ -12,24 +13,40 @@ package body RP.PIO.Touch_Sense is
 
    procedure Initialize (This       : in out Touch_Sensor;
                          ASM_Offset :        PIO_Address := 0;
-                         Max_Count  :        HAL.UInt32  := 10_000)
+                         Max_Count  :        HAL.UInt32  := 10_000;
+                         Mode       :        Touch_Mode  := Discharge)
    is
       Config : PIO_SM_Config := Default_SM_Config;
    begin
       This.Max_Count := Max_Count;
 
-      This.PIO.Load
-        (Prog   => Touch_Sense_PIO.Touch_Sense_Program_Instructions,
-         Offset => ASM_Offset);
+      case Mode is
+         when Discharge =>
+            This.PIO.Load
+              (Prog   => Touch_Sense_Discharge_Program_Instructions,
+               Offset => ASM_Offset);
+         when Charge =>
+            This.PIO.Load
+              (Prog   => Touch_Sense_Charge_Program_Instructions,
+               Offset => ASM_Offset);
+      end case;
 
       This.Pin.Configure (Output, Floating, This.PIO.GPIO_Function);
 
       Set_Jmp_Pin (Config, This.Pin.Pin);
       Set_Set_Pins (Config, This.Pin.Pin, 1);
 
-      Set_Wrap (Config,
-                ASM_Offset + Touch_Sense_PIO.Touch_Sense_Wrap_Target,
-                ASM_Offset + Touch_Sense_PIO.Touch_Sense_Wrap);
+      case Mode is
+         when Discharge =>
+            Set_Wrap (Config,
+                      ASM_Offset + Touch_Sense_Discharge_Wrap_Target,
+                      ASM_Offset + Touch_Sense_Discharge_Wrap);
+
+         when Charge =>
+            Set_Wrap (Config,
+                      ASM_Offset + Touch_Sense_Charge_Wrap_Target,
+                      ASM_Offset + Touch_Sense_Charge_Wrap);
+      end case;
 
       Set_Clock_Frequency (Config, 125_000_000);
       This.PIO.SM_Initialize (This.SM, ASM_Offset, Config);
