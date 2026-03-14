48f0fa6c62a36d83bc1ace6c4b26c265a67a00f3
Jeremy Grosser <jeremy@synack.me>
2026-03-14 15:28:08 -0700

    rp2350 tests

--- tests/src/test_suite.adb
+++ tests/src/test_suite.adb
@@ -3,8 +3,8 @@
 --
 --  SPDX-License-Identifier: BSD-3-Clause
 --
-with Clock_Tests;
-with GPIO_Tests;
+--  with Clock_Tests;
+--  with GPIO_Tests;
 with UART_Tests;
 --  with PWM_Tests;
 with DMA_Tests;
@@ -20,8 +20,8 @@ with Reset_Tests;
 package body Test_Suite is
    Result : aliased AUnit.Test_Suites.Test_Suite;
 
-   Clock_Case     : aliased Clock_Tests.Clock_Test;
-   GPIO_Case      : aliased GPIO_Tests.GPIO_Test;
+   --  Clock_Case     : aliased Clock_Tests.Clock_Test;
+   --  GPIO_Case      : aliased GPIO_Tests.GPIO_Test;
    UART_Case      : aliased UART_Tests.UART_Test;
    --  PWM_Case       : aliased PWM_Tests.PWM_Test;
    DMA_Case       : aliased DMA_Tests.DMA_Test;
@@ -39,8 +39,8 @@ package body Test_Suite is
    is
       use AUnit.Test_Suites;
    begin
-      Add_Test (Result'Access, Clock_Case'Access);
-      Add_Test (Result'Access, GPIO_Case'Access);
+      --  Add_Test (Result'Access, Clock_Case'Access);
+      --  Add_Test (Result'Access, GPIO_Case'Access);
       Add_Test (Result'Access, UART_Case'Access);
       --  Add_Test (Result'Access, PWM_Case'Access);
       Add_Test (Result'Access, DMA_Case'Access);
